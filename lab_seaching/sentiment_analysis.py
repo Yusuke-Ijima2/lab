be8115336a32fa809aa2c45ad78416118ce458d0
Yusuke-Ijima2 <yusuke9251120@gmail.com>
2022-12-12 15:24:04 +0900

    感情分析スクリプト作成

---
+++ lab_seaching/sentiment_analysis.py
@@ -0,0 +1,46 @@
+key = "20b487e731954ee683ddba763fe40c53"
+endpoint = "https://tubokawa-lab-research2.cognitiveservices.azure.com/"
+
+# 各種ライブラリのインポート
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.core.credentials import AzureKeyCredential
+
+# キーとエンドポイントを利用してクライアントの認証を行います 
+def authenticate_client():
+    ta_credential = AzureKeyCredential(key)
+    text_analytics_client = TextAnalyticsClient(
+        endpoint=endpoint, 
+        credential=ta_credential)
+    return text_analytics_client
+
+client = authenticate_client()
+
+# Example function for detecting sentiment in text
+def sentiment_analysis_example(client):
+    documents = [
+        {
+            "id": "1",
+            "language": "ja",
+            "text": "機能が充実していて使いやすい。デザインが好みではない。"
+        },
+    ]
+
+    response = client.analyze_sentiment(documents=documents)
+
+    for document in response:
+        print("Document Id: ", document.id)
+        print("Document Sentiment: {}".format(document.sentiment))
+        print("Overall scores: positive={0:.2f}; neutral={1:.2f}; negative={2:.2f} \n".format(
+            document.confidence_scores.positive,
+            document.confidence_scores.neutral,
+            document.confidence_scores.negative,
+        ))
+        for idx, sentence in enumerate(document.sentences):
+            print("Sentence: {}".format(sentence.text))
+            print("Sentence {} sentiment: {}".format(idx+1, sentence.sentiment))
+            print("Sentence score:\nPositive={0:.2f}\nNeutral={1:.2f}\nNegative={2:.2f}\n".format(
+                sentence.confidence_scores.positive,
+                sentence.confidence_scores.neutral,
+                sentence.confidence_scores.negative,
+            ))
+sentiment_analysis_example(client)
