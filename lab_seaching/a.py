08ac3fed4bb26b99f39cfdd37224924e61540d56
Yusuke-Ijima2 <yusuke9251120@gmail.com>
2023-01-11 13:07:32 +0900

    その他

---
+++ lab_seaching/a.py
@@ -0,0 +1,3 @@
+with open('q_sentiment_test.txt', 'r') as f:
+    q_result_list = f.read().split("\n")
+print(len(q_result_list))
--- test.py
+++ test.py
@@ -1,12 +1,15 @@
-research_array = []
-array = ["イケメン。","ブス。"]
-for i in range(len(array)):
-    documents = [
-        {
-            "id": i + 1,
-            "language": "ja",
-            "text": array[i]
-        }
-    ]
-    research_array.append(documents)
-print(research_array)
+# research_array = []
+# array = ["イケメン。","ブス。"]
+# for i in range(len(array)):
+#     documents = [
+#         {
+#             "id": i + 1,
+#             "language": "ja",
+#             "text": array[i]
+#         }
+#     ]
+#     research_array.append(documents)
+# print(research_array)
+
+for i in range(20, 30):
+    print(i)
