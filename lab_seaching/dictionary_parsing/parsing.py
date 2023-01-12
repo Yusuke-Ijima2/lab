1eb76deefaaa6dc7af992eee93934ae0aa90a1bd
Yusuke-Ijima2 <yusuke9251120@gmail.com>
2023-01-12 16:10:08 +0900

    意味がわからない単語を削除

--- lab_seaching/dictionary_parsing/parsing.py
+++ lab_seaching/dictionary_parsing/parsing.py
@@ -1,19 +1,26 @@
 from janome.tokenizer import Tokenizer
 from wordcloud import WordCloud
 
+import collections
+
 with open('lab_seaching/q_sentiment_test.txt', 'r') as f:
     parsing_list = f.read().split("\n")
     # print(parsing_list)
 
 t = Tokenizer()
-results = []
+results = []    
 for parsing in parsing_list:
-    # 全角スペースを削除 # リストに追加
-    tokens = list(t.tokenize(parsing, wakati=True))
-    result = [i.replace('\u3000','') for i in tokens] 
+    # 名詞、動詞、形容詞を切り替えて使う
+    result = [token.base_form for token in t.tokenize(parsing) if token.part_of_speech.split(',')[0] in ['形容詞']]
     results.extend(result)
 
+# 単語の頻度を確認
+c = collections.Counter(results)
+print(c.most_common())
 print(results)
+
+# 削除するワードをリスト化
+stopwords = ['ん', 'よう', 'の', 'こと', 'さ', 'そう', 'まま', 'はず']
 
 # wordcloud
 #日本語のフォントパス
@@ -21,6 +28,6 @@ fpath = '/Library/Fonts//Arial Unicode.ttf'
 text = ' '.join(results) # 区切り文字を「・」にして文字列に変換
 print(text)
 # 単語の最大表示数は500に設定
-wordcloud = WordCloud(background_color='white', font_path=fpath, width=800, height=600, max_words=500).generate(text)
+wordcloud = WordCloud(background_color='white', font_path=fpath, width=800, height=600, max_words=500, stopwords=set(stopwords)).generate(text)
 print(wordcloud)
 wordcloud.to_file('./wordcloud.png')
